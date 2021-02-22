f7a39a49d8dac0e21b6f9693fdd4e6e013a0ccb2
Jacques Desmarais <jacques.desmarais@ssense.com>
2021-02-21 21:57:40 -0500

    fix issue with wallaby "expect is not defined" by adding "autoDetect: true" to wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -1,10 +1,15 @@
-module.exports = function () {
-    process.env.TZ = 'UTC';
+process.env.TZ = 'UTC';
+
+module.exports = function (wallaby) {
     return {
         files: ['src/**/*.ts'],
         tests: ['tests/**/*.test.ts'],
         env: {
             type: 'node',
+        },
+        autoDetect: true,
+        compilers: {
+            '**/*.ts?(x)': wallaby.compilers.typeScript({ isolatedModules: true }),
         },
     };
 };
